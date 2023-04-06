b6be944d976d073e353b5b7554ee96517d6ae850
miyo <cuteboycore@gmail.com>
2023-04-06 13:07:18 -0300

    3 commit

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ let num2= 2 ;
 let res= num1%num2 ;
 console.log(res);
 
+//exercicio 3
+let texto= prompt('informe seu imc!')
+if(texto <18.5){
+    document.write('voce esta abaixo do imc adequado')
+    document.write("<br><br>")
+}
+
+
+if(texto == 18 || texto <=24){
+    document.write('seu imc esta adequado')
+    document.write("<br><br>")
+
+}
+else if(texto >= 25){
+    document.write('seu imc esta fora do normal por favor consultar o medico')
+    document.write("<br><br>")
+}
+
